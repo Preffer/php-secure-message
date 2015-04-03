13b2a2cd813be3d1d3e9938583e3a9bb5aabbdeb
Yuzo <hyzgog@gmail.com>
2015-04-03 14:45:19 +0800

    use exception

--- security.php
+++ security.php
@@ -1,40 +1,46 @@
 <?php
+set_error_handler(function($severity, $message, $file, $line) {
+	throw new ErrorException($message, $severity, $severity, $file, $line);
+});
 
-set_error_handler(function($severity, $message, $filename, $lineno) {
+try {
+	switch ($GLOBALS['argv'][1]) {
+		case 'all':
+			$names = generate(['alice', 'bob']);
+			$signed = sign('sample.pdf', $names[0]);
+			$encrypted = encrypt($signed, $names[1]);
+			break;
+
+		case 'generate':
+			generate(array_slice($GLOBALS['argv'], 2));
+			break;
+
+		case 'sign':
+			sign($GLOBALS['argv'][2], $GLOBALS['argv'][3]);
+			break;
+
+		case 'encrypt':
+			encrypt($GLOBALS['argv'][2], $GLOBALS['argv'][3]);
+			break;
+
+		case 'decrypt':
+			decrypt($GLOBALS['argv'][2], $GLOBALS['argv'][3]);
+			break;
+
+		default:
+			throw new Exception('Unknown action');
+	}
+} catch (Exception $e) {
 	$filename = $GLOBALS['argv'][0];
+	$message = $e->getMessage();
 
-	echo("{$message}\n");
+	echo("$message\n");
 	echo("Usage: php {$filename} {all|generate|sign|encrypt|decrypt|verify} [arg]\n");
 	echo("Example:\n");
 	echo("\tphp {$filename} generate alice bob\n");
 	echo("\tphp {$filename} sign sample.pdf alice\n");
-	echo("\tphp {$filename} encrypt sample.pdf.sign.tar bob\n");
-	exit(1);
-});
-
-switch ($GLOBALS['argv'][1]) {
-	case 'all':
-		$names = generate(['alice', 'bob']);
-		$signed = sign('sample.pdf', $names[0]);
-		$encrypted = encrypt($signed, $names[1]);
-		break;
-
-	case 'generate':
-		generate(array_slice($GLOBALS['argv'], 2));
-		break;
-
-	case 'sign':
-		sign($GLOBALS['argv'][2], $GLOBALS['argv'][3]);
-		break;
-
-	case 'encrypt':
-		encrypt($GLOBALS['argv'][2], $GLOBALS['argv'][3]);
-		break;
-
-	default:
-		trigger_error('Unknown action');
+	echo("\tphp {$filename} encrypt sample.pdf.sign bob\n");
 }
-
 
 function generate($names) {
 	if (empty($names)) {
@@ -56,32 +62,32 @@ function generate($names) {
 	return $names;
 }
 
-function sign($file, $name) {
+function sign($file, $author) {
 	$data = file_get_contents($file);
-	$private = file_get_contents("{$name}_private.pem");
+	$private = file_get_contents("{$author}_private.pem");
 
 	openssl_sign($data, $signature, $private);
 
-	$pack = new PharData("{$file}.{$name}.tar");
+	$pack = new PharData("{$file}.sign");
 	$pack->addFile($file);
-	$pack->addFromString("{$file}.{$name}", $signature);
+	$pack->addFromString("sign", $signature);
 
-	echo("Signed as {$name}: {$file}.{$name}.tar\n");
-	return "{$file}.{$name}.tar";
+	echo("Signed as {$author}: {$file}.sign\n");
+	return "{$file}.sign";
 }
 
-function encrypt($file, $name) {
+function encrypt($file, $recipient) {
 	$data = file_get_contents($file);
-	$public = file_get_contents("{$name}_public.pem");
+	$public = file_get_contents("{$recipient}_public.pem");
 
 	openssl_seal($data, $sealed, $keys, [$public]);
 
-	$pack = new PharData("{$file}.${name}.tar");
-	$pack->addFromString("{$file}.${name}", $sealed);
-	$pack->addFromString("{$file}.key", $keys[0]);
+	$pack = new PharData("{$file}.encrypt");
+	$pack->addFromString("{$file}", $sealed);
+	$pack->addFromString("key", $keys[0]);
 
-	echo("Encrypted for ${name}: {$file}.${name}.tar\n");
-	return "{$file}.{$name}.tar";
+	echo("Encrypted for ${recipient}: {$file}.encrypt\n");
+	return "{$file}.encrypt";
 }
 
 ?>
